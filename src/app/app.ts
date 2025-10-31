6a9334fc7c94f10892a070174728efe8437bfc2b
Chetan Kumar <chetank9903@gmail.com>
2025-10-31 11:36:59 +0530

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Banking_Payment_App_Frontend');
+}
